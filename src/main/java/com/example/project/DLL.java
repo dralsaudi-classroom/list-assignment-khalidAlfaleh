ee10391d1871d26479478a1b8a766eeb39ecaede
khalidAlfaleh <444102210@student.ksu.edu.sa>
2024-09-27 20:06:34 +0300

    Update DLL.java

--- src/main/java/com/example/project/DLL.java
+++ src/main/java/com/example/project/DLL.java
@@ -80,6 +80,7 @@ public class DLL<T> {
 			      
 			    runner2 = runner2.next;
 		    }
+	    if((runner1 == null) && (runner2 == null)){
 		    if( (runner1.data == e1) && (runner2.data == e2) ){
 			    current = runner1.next;
 			    while(current != runner2){
@@ -87,13 +88,7 @@ public class DLL<T> {
 				    current = current.next;
 			    }
 			    current = head;
-		    
-		    
-
-
-
-
-		    
+	    }
 	    }
 		    
 
