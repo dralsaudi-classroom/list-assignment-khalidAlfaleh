8402fff0a20f3e07fab09e8182a5029ce11736e9
khalidAlfaleh <444102210@student.ksu.edu.sa>
2024-09-27 20:11:46 +0300

    Update DLL.java

--- src/main/java/com/example/project/DLL.java
+++ src/main/java/com/example/project/DLL.java
@@ -87,8 +87,8 @@ public class DLL<T> {
 				    remove();
 				    current = current.next;
 			    }
-			    current = head;
-	    }
+                           findFirst() 	
+				   }
 	    }
 		    
 
